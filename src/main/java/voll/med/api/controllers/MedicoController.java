74e3bd703c55aad4d9999ee4e4f43bfdfc5fda65
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-09-28 17:15:30 -0300

    adicionando a migration v3 e implementacao do metodo excluir logico

--- src/main/java/voll/med/api/controllers/MedicoController.java
+++ src/main/java/voll/med/api/controllers/MedicoController.java
@@ -1,7 +1,5 @@
 package voll.med.api.controllers;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,7 +28,6 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
 
-
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
@@ -39,22 +36,32 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size=10,sort={"nome"}) Pageable paginacao) {   
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
 
+    /*
+     * @DeleteMapping("/{id}")
+     * 
+     * @Transactional
+     * public void excluir(@PathVariable Long id){
+     * repository.deleteById(id);
+     * 
+     * }
+     */
 
     @DeleteMapping("/{id}")
-    @Transactional
-    public void excluir(@PathVariable Long id){
-        repository.deleteById(id);
+    @Transactional 
+    public void excluirLogico(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluirLogico();
 
     }
 
--- src/main/java/voll/med/api/medicos/Medico.java
+++ src/main/java/voll/med/api/medicos/Medico.java
@@ -35,8 +35,10 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo =true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -60,6 +62,10 @@ public class Medico {
             this.endereco.atualizarInformacoes(dados.endereco());
             
         }
+    }
+
+    public void excluirLogico() {
+        this.ativo=false;
     }
 
 }
