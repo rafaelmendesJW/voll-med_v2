b00a2ce7bc2dded311b5dbcf305e0d4d33913c48
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-10-09 16:40:13 -0300

    Implementando método detalhar nos controllers

--- src/main/java/voll/med/api/controllers/MedicoController.java
+++ src/main/java/voll/med/api/controllers/MedicoController.java
@@ -66,6 +66,13 @@ public class MedicoController {
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
+    }
+
     /*
      * @DeleteMapping("/{id}")
      * 
--- src/main/java/voll/med/api/controllers/PacienteController.java
+++ src/main/java/voll/med/api/controllers/PacienteController.java
@@ -70,6 +70,13 @@ public class PacienteController {
         return ResponseEntity.noContent().build();
     }
 
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
+
     /*
      * @GetMapping
      * public List<Paciente> listar() {
