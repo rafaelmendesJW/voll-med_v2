6f4f0db170de2e6041b3fb25800f13083ea1962b
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-10-10 20:22:53 -0300

    Criando o Usuario Repository

---
+++ src/main/java/voll/med/api/domain/usuarios/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package voll.med.api.domain.usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository  extends JpaRepository <Usuario,Long>{
+    
+}
