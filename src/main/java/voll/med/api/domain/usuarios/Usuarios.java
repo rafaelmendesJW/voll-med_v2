164c8830f543641b85ed3e2b89b80158cb8026bb
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-10-10 20:07:53 -0300

    Implementação da entidade Usuario

---
+++ src/main/java/voll/med/api/domain/usuarios/Usuarios.java
@@ -0,0 +1,27 @@
+package voll.med.api.domain.usuarios;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;    
+}
